bfb6282fde1184dc5f8660ab6980abc2c51f11ea
Majid Askarifarsangi (Elev) <majidas@viken.no>
2024-06-15 14:46:27 +0200

    config ThemeProvider

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,15 @@ const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
       <div>
-        <Spotlight className='-top-40 -left-10 md:-left-32 md:top-20 h-screen' fill='white'/>        
-        <Spotlight className='-top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>        
-        <Spotlight className='top-28 -left-80 h-[80vh] w-[50vw]' fill='blue'/>        
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple"/>
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+
+    </div>
     </div>
   )
 }
