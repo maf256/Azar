f8eea38bbab93200cffccc329d4ebb2a7d11ccb3
Majid Askarifarsangi (Elev) <maf256@gmail.com>
2024-06-20 22:54:23 +0200

    Added Data index.ts

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/Data'
 
 const Grid = () => {
   return (
     <section id= "about">
         <BentoGrid>
-            {[{title: "Title1", description: "Desc11", id: 1}
-            ,{title: "Title2", description: "Desc12", id: 2}
-            ,{title: "Title3", description: "Desc13", id: 3}
-            ,{title: "Title4", description: "Desc14", id: 4}]
-            .map((item) => (
+            {gridItems.map((item) => (
                 <BentoGridItem 
                     key={item.id}
                     id= {item.id}
