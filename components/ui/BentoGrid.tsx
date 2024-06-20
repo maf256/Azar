d85a881fedcc463205139dc07692a84258f72837
Majid Askarifarsangi (Elev) <maf256@gmail.com>
2024-06-21 00:18:13 +0200

    working on BentoGrid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -47,10 +47,21 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        background: 'rgba(4,7,29,)',
-        backgroundColor: 'gradient...',
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
+      <div className={`${id === 6 && 'flex justify-center h-full'}`}>
+        <div className="w-full  h-full absolute">
+          <img src={img} alt={img} className={cn(imgClassName, 'object-cover object-center')} />
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          <img src={spareImg} alt={spareImg} className="object-cover object-center w-full h-full" />
+        </div>
+
+
+      </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
