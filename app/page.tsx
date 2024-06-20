0837747c26c2ec317b4026b3a6d50e8b51210b63
Majid Askarifarsangi (Elev) <majidas@viken.no>
2024-06-20 10:18:55 +0200

    add h1 in page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <main className="relative bg-black-100 flex juxtify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
           <Hero />
+          <h1>hello world</h1>
         </div>
       </main>
     );
